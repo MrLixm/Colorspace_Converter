1dbe846ff8fad3b4d5d37b539190494cd8db4b3a
Liam <lcollod@gmail.com>
2020-06-06 19:52:56 +0200

    Creation of list/dico for options

---
+++ src/main/python/package/data_list.py
@@ -0,0 +1,11 @@
+CS_TARGET_LIST = ['ACEScg', 'ACES2065-1', 'ACEScc', 'ACEScct', 'sRGB', 'Adobe Wide Gamut RGB',
+                  'ITU-R BT.2020', ]
+
+FORMAT_LIST = [".exr", ".jpg", ".png"]
+BITDEPTH_DICO = {
+    "8bit Int": "uint8",
+    "16bit Int": "uint16",
+    "16bit Half": "half",
+    "32bit Float": "float",
+    "Original": "original"
+}
